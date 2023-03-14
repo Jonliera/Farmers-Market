58df37601256b596d1ddc610919671b3233dabf2
Jonathan Liera <jonliera1@gmail.com>
2023-03-13 20:59:48 -0700

    add the beginning of the project and basic components
    Co-authored-by: Grace Lee <gracelee9901@gmail.com>
    Co-authored-by: Greg Ramsower <greg.ramsower@gmail.com>
    Co-authored-by: Rafael Petrachini <raffp@me.com>
    Co-authored-by: Remy Flores <remyfranciscoflores@gmail.com>

---
+++ src/components/MarketControl.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class MarketControl extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <React.Fragment></React.Fragment>;
+  }
+}
+
+export default MarketControl;
